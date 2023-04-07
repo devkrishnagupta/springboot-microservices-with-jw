b2aa9c871b0f445f72addf05695538b2d708814c
Aaron Barnes <dev1.entnest@gmail.com>
2023-04-08 00:38:56 +0530

    removed hostname and port & instead used service name as registered in service registry

--- UserService/src/main/java/com/devkrishnagupta/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/devkrishnagupta/user/service/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.devkrishnagupta.user.service.controllers;
 
+import com.devkrishnagupta.user.service.entity.Hotel;
 import com.devkrishnagupta.user.service.entity.Rating;
 import com.devkrishnagupta.user.service.entity.User;
 import com.devkrishnagupta.user.service.services.UserService;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/users")
@@ -41,9 +44,22 @@ public class UserController {
         User user = userService.getUser(userId);
         //fetch the rating of the above user from RATING-SERVICE
         //http://localhost:8083/api/ratings/user/63277621-4488-4457-afd5-0017164bbac7
-        ArrayList<Rating> ratings = restTemplate.getForObject("http://localhost:8083/api/ratings/user/"+userId, ArrayList.class);
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERIVE/api/ratings/user/"+userId, Rating[].class);
+        List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
         logger.info("Ratings with user id {} is {}", userId, ratings);
-        user.setRatings(ratings);
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            //API call to hotel service to get the hotel
+            http://localhost:8082/api/hotels/34fb1d0a-8318-472a-a0c8-a01ec15d2dd8
+//            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/api/hotels/" + rating.getHotelId(), Hotel.class);
+//            Hotel hotel = forEntity.getBody();
+            //set the hotel to rating
+//            rating.setHotel(hotel);
+            rating.setHotel(restTemplate.getForEntity("http://HOTEL-SERVICE/api/hotels/" + rating.getHotelId(), Hotel.class).getBody());
+            //return the rating
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
         return ResponseEntity.ok(user);
     }
 
